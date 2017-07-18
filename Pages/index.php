19b8c1440ce62c97a177cfc18e26ed00327e5ad2
Kurt Ebol <kurt_ebol@dlsu.edu.ph>
2017-07-19 02:31:14 +0800

    Finished all algorithms, 100% working

--- Pages/index.php
+++ Pages/index.php
@@ -1,96 +1,100 @@
 <html>
     <head>
+        <title>SYSINTG Voter Sorter</title>
         <script src="../plugins/jQuery/jquery-2.2.3.min.js"></script>
         <script src="http://www.google.com/jsapi" type="text/javascript"></script>
         <script src="../plugins/angular/angular.js"></script>
         <script src="../plugins/angular/sampleapp.js"></script>
     </head>
-    <body ng-app="sampleapp" ng-controller="MainController" ng-init="<?php
-                            
-                            require_once('../mysql_connect.php');
-                            $query = "SELECT *, DATEDIFF(CURDATE(), birthday)DIV 365 AS age 
-                                        FROM univdata;";
-                            $result = mysqli_query($dbc,$query);
-                            $ctr = 1;
-                            if($result){
-                                 echo 'students=[';
-                                 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                                        echo "
-                                        {firstName:'{$row['name']}', ";
-                                        echo "lastName:'{$row['surname']}', ";
-                                        echo "birthday:'{$row['birthday']}', ";
-                                        echo "university:'{$row['university']}', ";
-                                        echo "age:'{$row['age']}'}";
-                                        if($ctr <= mysqli_num_rows($result) - 1) echo ", ";
-                                        else echo "]; ";
-                                        $ctr++;
-                                 }
-                            };                      
-                                                                     
-                            require_once('../mysql_connect.php');
-                            $query = "SELECT university, COUNT(university) as UNIVCOUNT
-                                        FROM univdata
-                                       GROUP BY UNIVERSITY;";
-                            $result = mysqli_query($dbc,$query);
-                            $ctr = 1;
-                            if($result){
-                                 echo ' univCount=[';
-                                 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                                    echo "{$row['UNIVCOUNT']}";
-                                    if($ctr <= mysqli_num_rows($result) - 1) echo ", ";
-                                    else echo "]; ";
-                                    $ctr++;
-                                 }
-                            };
-                            
-                            require_once('../mysql_connect.php');
-                            $query = "SELECT COUNT(university) as UNIVCOUNT
-                                        FROM univdata;";
-                            $result = mysqli_query($dbc,$query);
-                            if($result){
-                                 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                                    echo " totalCount={$row['UNIVCOUNT']}; ";
-                                 }
-                            };
-                                               
-                            require_once('../mysql_connect.php');
-                            $query = "SELECT DATEDIFF(CURDATE(), birthday)DIV 365 AS AGE
-                                        FROM univdata
-                                       GROUP BY 1;";
-                            $result = mysqli_query($dbc,$query);
-                            $ctr = 1;
-                            if($result){
-                                 echo ' ageArray=[';
-                                 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                                    echo "{$row['AGE']}";
-                                    if($ctr <= mysqli_num_rows($result) - 1) echo ", ";
-                                    else echo "];";
-                                    $ctr++;
-                                 }
-                            };
-                         ?>">
-        <label>University/ies:</label>
-            <div>
+    <body ng-app="sampleapp" ng-controller="MainController" ng-init="
+    <?php                            
+        require_once('../mysql_connect.php');
+        $query = "SELECT *, DATEDIFF(CURDATE(), birthday)DIV 365 AS age 
+                    FROM univdata;";
+        $result = mysqli_query($dbc,$query);
+        $ctr = 1;
+        if($result){
+             echo 'students=[';
+             while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                    echo "
+                    {firstName:'{$row['name']}', ";
+                    echo "lastName:'{$row['surname']}', ";
+                    echo "birthday:'{$row['birthday']}', ";
+                    echo "university:'{$row['university']}', ";
+                    echo "age:'{$row['age']}'}";
+                    if($ctr <= mysqli_num_rows($result) - 1) echo ", ";
+                    else echo "]; ";
+                    $ctr++;
+             }
+        };                      
+
+        require_once('../mysql_connect.php');
+        $query = "SELECT university, COUNT(university) as UNIVCOUNT
+                    FROM univdata
+                   GROUP BY UNIVERSITY;";
+        $result = mysqli_query($dbc,$query);
+        $ctr = 1;
+        if($result){
+             echo ' univCount=[';
+             while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                echo "{$row['UNIVCOUNT']}";
+                if($ctr <= mysqli_num_rows($result) - 1) echo ", ";
+                else echo "]; ";
+                $ctr++;
+             }
+        };
+
+        require_once('../mysql_connect.php');
+        $query = "SELECT COUNT(university) as UNIVCOUNT
+                    FROM univdata;";
+        $result = mysqli_query($dbc,$query);
+        if($result){
+             while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                echo " totalCount={$row['UNIVCOUNT']}; ";
+             }
+        };
+
+        require_once('../mysql_connect.php');
+        $query = "SELECT DATEDIFF(CURDATE(), birthday)DIV 365 AS AGE
+                    FROM univdata
+                   GROUP BY 1;";
+        $result = mysqli_query($dbc,$query);
+        $ctr = 1;
+        if($result){
+             echo ' ageArray=[';
+             while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                echo "{$row['AGE']}";
+                if($ctr <= mysqli_num_rows($result) - 1) echo ", ";
+                else echo "];";
+                $ctr++;
+             }
+        };
+    ?>">
+        <h3><b>University/ies:</b></h3>
+        <div>
             <form>
-                <input name="univ" type="radio" value="all" ng-model="showUniv" ng-click="refreshArray()" checked="checked">All (Total Students: <font color="red"><b>{{totalCount}}</b></font>)
-                <input name="univ" type="radio" value="multi" ng-model="showUniv" ng-click="refreshArray()">Select University
+                <input name="univ" type="radio" value="all" ng-model="showUniv" ng-click="clearUniv(); refreshArray()" checked>All (Total Students: <font color="red"><b>{{totalCount}}</b></font>)
+                <input name="univ" type="radio" value="multi" ng-model="showUniv" ng-click="undoUniv(); refreshArray()">Select University
             </form>
-            </div>
-            <div ng-show="showUniv == 'multi'" >
+        </div>
+        <div ng-show="showUniv == 'multi'">
             <form>
                 <label ng-repeat="univ in universities">
                     <input type="checkbox" name="selectedUnivs[]" value="{{univ}}" ng-click="toggleSelection(univ)"> {{univ}} <font color="red"><b>({{univCount[$index]}})</b></font>
                 </label>
-                <br><br>
+                <br>
+                <br>
             </form>
-            </div>
+        </div>
         <label>Age: </label>
-        <select name="age">
-            <option value="0" ng-selected="refreshArray(-1)">--</option>
-            <option value="{{x}}" ng-repeat="x in ageArray" ng-selected="refreshArray(x)">{{x}}</option>
+        <select name="age" id="ageSelect">
+            <option value="0">--</option>
+            <option value="{{x}}" ng-repeat="x in ageArray">{{x}}</option>
         </select>
-        <button>Sort by Last Name</button>
-        <br><br>
+        <button ng-click="sortByLastName()">Sort by Last Name</button>
+        <button ng-click="refreshArray()">Group By Age</button>
+        <br>
+        <br>
         <div>
             <table id="univtable" class="display" border="2" cellspacing="0" width="100%" margin>
                 <thead>
--- plugins/angular/sampleapp.js
+++ plugins/angular/sampleapp.js
@@ -4,43 +4,65 @@ app.controller('MainController', function ($scope) {
     $scope.totalCount,
     $scope.ageArray = [],
     $scope.univCount = [],
+    $scope.sorted = false,
     $scope.universities = ['ADMU', 'DLSU', 'LPU', 'MIT', 'STI', 'UP', 'UST'],
     $scope.studentsSubset = [],
     $scope.selectedUniv = [],
+    $scope.temp = [];
+    $scope.clearUniv = function clearUniv() {
+        $scope.temp = $scope.selectedUniv;
+        $scope.selectedUniv = [];
+    },
+    $scope.undoUniv = function clearUniv() {
+        $scope.selectedUniv = $scope.temp;
+    },
     $scope.toggleSelection = function toggleSelection(univ) {
-    var idx = $scope.selectedUniv.indexOf(univ);
-
-    // Is currently selected
-    if (idx > -1) {
-      $scope.selectedUniv.splice(idx, 1);
-    }
-
-    // Is newly selected
-    else {
-      $scope.selectedUniv.push(univ);
-    }
-    
-    $scope.refreshArray();
-  },
+        var idx = $scope.selectedUniv.indexOf(univ);
+        if (idx > -1) {
+            $scope.selectedUniv.splice(idx, 1);
+        } else {
+            $scope.selectedUniv.push(univ);
+        }
+        $scope.refreshArray();
+    },
     $scope.refreshArray = function refreshArray() {
         $scope.studentsSubset = [];
-        for(var x = 0; x < $scope.students.length; x++){
-            if($scope.selectedUniv.length !== null){
-                for(var y = 0; y < $scope.selectedUniv.length; y++) {
-                    if($scope.students[x].university == $scope.selectedUniv[y]){
+        for (var x = 0; x < $scope.students.length; x++) {
+            if ($scope.selectedUniv.length > 0) {
+                for (var y = 0; y < $scope.selectedUniv.length; y++) {
+                    if ($scope.students[x].university == $scope.selectedUniv[y]) {
                         $scope.studentsSubset.push($scope.students[x]);
                     }
-                }                
+                }
             } else {
                 $scope.studentsSubset = $scope.students;
             }
         }
-    },
-    $scope.refreshArray = function refreshArray(age) {
-        for(var x = 0; x < $scope.studentsSubset.length; x++){
-            if($scope.studentsSubset[x].age == age){
-                $scope.studentsSubset.push($scope.students[x]);
+        var selectedAge = document.getElementById("ageSelect").options[document.getElementById("ageSelect").selectedIndex].value;
+        if (selectedAge > 0) {
+            var tempArray = [];
+            for (var x = 0; x < $scope.studentsSubset.length; x++) {
+                if ($scope.studentsSubset[x].age == selectedAge) {
+                    tempArray.push($scope.studentsSubset[x]);
+                }
             }
+            $scope.studentsSubset = tempArray;
         }
+    },
+    $scope.sortByLastName = function sortByLastName() {
+        if (!$scope.sorted) {
+            $scope.students.sort(function (a, b) {
+                if (a.lastName < b.lastName)
+                    return -1;
+                if (a.lastName > b.lastName)
+                    return 1;
+                return 0;
+            });
+            $scope.sorted = true;
+        } else {
+            $scope.students.reverse();
+            $scope.sorted = false;
+        }
+        $scope.refreshArray();
     }
 });
